96c7f1e8d8c41e259e73c2f64bd27690a0b4cdb8
Russell Sherman <russell.t.sherman@gmail.com>
2016-07-20 07:51:02 -0500

    add named function to foreach

--- 01-foreach.js
+++ 01-foreach.js
@@ -3,7 +3,7 @@ var colors = [ 'red', 'blue', 'green' ];
 // typical for loop              
 
 for (var i = 0; i < colors.length; i++) {
-  console.log(colors[i])
+	console.log(colors[i])
 }
 
 // forEach results in the same action as a for loop 
@@ -12,3 +12,11 @@ for (var i = 0; i < colors.length; i++) {
 colors.forEach(function(color){
 	console.log(color);
 });
+
+// you can also use named functions
+// named functons make stacktraces easier to read
+
+function logColor(color) {
+	console.log(color);
+}
+colors.forEach(logColor);
