ded934aa61dd38c6409fcc51b2c3e66050d7f376
Russell Sherman <russell.t.sherman@gmail.com>
2016-07-21 08:04:00 -0500

    add template strings

---
+++ 09-template-strings.js
@@ -0,0 +1,9 @@
+
+const name = 'bob';
+const email = 'bob@example.com';
+
+// typical string concatenation
+let message = 'You can reach ' + name + ' at ' + email;
+
+// using template strings
+let message = `you can reach ${name} at ${email}`
