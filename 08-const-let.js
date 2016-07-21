ae74ef36356bb2bc12819cb73c6452515c80bdab
Russell Sherman <russell.t.sherman@gmail.com>
2016-07-21 08:00:05 -0500

    add const

---
+++ 08-const-let.js
@@ -0,0 +1,11 @@
+// typical variable declarations
+var name = 'bob';
+var age = 42;
+
+// using const / let
+const name = 'bob';
+let age = 42;
+
+// const is for variables which we do not expect to change
+// let is for variables which we do expect to change
+
