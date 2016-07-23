6bcc45c2f5e5fec37d18c495d979f5775d9037f6
Russell Sherman <russell.t.sherman@gmail.com>
2016-07-23 11:58:35 -0500

    add example

--- 18-symbol-iterator.js
+++ 18-symbol-iterator.js
@@ -1,5 +1,4 @@
 // symbol iterator is a tool that teaches an object how to respond to the for of loop
-
 const testingTeam = {
   lead: 'Amanda',
   tester: 'Bill',
@@ -28,3 +27,30 @@ for(let name of engineeringTeam) {
   names.push(name);
 }
 names; // ["Jill"," Alex","Dave","Amanda","Bill"]
+
+//-------------------------------
+// symbol iterator recursion
+class Comment {
+    constructor(content, children) {
+    this.content = content;
+    this.children = children;
+  }
+  *[Symbol.iterator]() {
+    yield this.content;
+    for (let child of this.children) {
+      yield* child;
+    }
+  }
+}
+const children = [
+  new Comment('good comment', []),
+  new Comment('bad comment', []),
+  new Comment('comment', [])
+]
+const tree = new Comment('Repost', children);
+
+const comments = [];
+for (let node of tree) {
+  comments.push(node);
+}
+comments; // ["Repost","good comment","bad comment","comment"]
